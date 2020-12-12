e041171456da1e6f254a057fdbcbea14d3c4ab0e
Gleb <gslepenkov@gmail.com>
2020-12-12 23:10:40 +0300

    Course finished

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,7 +9,7 @@ import About from "./AboutComponent";
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import Contact from "./ContactComponent";
 import { connect } from 'react-redux';
-import { postComment, fetchComments, fetchDishes, fetchPromos } from '../redux/ActionCreators';
+import { postComment, fetchComments, fetchDishes, fetchPromos, fetchLeaders, postFeedback } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
@@ -27,7 +27,9 @@ const mapDispatchToProps = (dispatch) => ({
     fetchDishes: () => { dispatch(fetchDishes()) },
     resetFeedbackForm: () => { dispatch(actions.reset('feedback')) },
     fetchComments: () => { dispatch(fetchComments()) },
-    fetchPromos: () => { dispatch(fetchPromos()) }
+    fetchPromos: () => { dispatch(fetchPromos()) },
+    fetchLeaders: () => { dispatch(fetchLeaders()) },
+    postFeedback: (Feedback) => dispatch(postFeedback(Feedback))
 });
 
 class Main extends React.Component {
@@ -40,7 +42,7 @@ class Main extends React.Component {
         this.props.fetchDishes();
         this.props.fetchComments();
         this.props.fetchPromos();
-
+        this.props.fetchLeaders();
     }
 
     onDishSelect(dishId) {
@@ -56,7 +58,9 @@ class Main extends React.Component {
                     promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
                     promosLoading={this.props.promotions.isLoading}
                     promosErrMess={this.props.promotions.errMess}
-                    leader={this.props.leaders.filter((leader) => leader.featured)[0]} />
+                    leader={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+                    leaderLoading={this.props.leaders.isLoading}
+                    leaderErrMess={this.props.leaders.errMess} />
             );
         }
         const DishWithId = ({ match }) => {
@@ -81,9 +85,9 @@ class Main extends React.Component {
                         <Switch>
                             <Route path={"/home"} component={HomePage} />
                             <Route exact path={"/menu"} component={() => <Menu dishes={this.props.dishes} />} />
-                            <Route exact path={"/contactus"} component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+                            <Route exact path={"/contactus"} component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} postFeedback={this.props.postFeedback} />} />
                             <Route path={"/menu/:dishId"} component={DishWithId} />
-                            <Route exact path={"/aboutus"} component={() => <About leaders={this.props.leaders} />} />
+                            <Route exact path={"/aboutus"} component={() => <About leaders={this.props.leaders.leaders} errMess={this.props.leaders.errMess} isLoading={this.props.leaders.isLoading} />} />
                             <Redirect to={"/home"} />
                         </Switch>
                     </CSSTransition>
